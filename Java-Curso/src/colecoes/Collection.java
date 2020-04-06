6fe92c48aedbcb7cd6343043b34de5a9d8d2dd8a
GussAlves <gus.alves.araujo@gmail.com>
2020-04-06 11:03:23 -0300

    Aprendendo conceitos de Collections

---
+++ Java-Curso/src/colecoes/Collection.java
@@ -0,0 +1,28 @@
+package colecoes;
+
+public class Collection {
+
+	/**
+	 * Set
+	 * 	- Não Ordenada (por Padrão)
+	 *  - Não indexado
+	 *  - Não aceita repetição
+	 *  
+	 * List 
+	 * 	- Indexada
+	 * 	- Aceita Repetição
+	 *
+	 * Map
+	 * 	- Chave/Valor
+	 * 	- Chave não aceita Repetição
+	 *  - Valor aceita repetição
+	 *  
+	 * Queue
+	 * 	- Implementa fila
+	 *  - First in/First out (fifo)
+	 *  
+	 * Stack 
+	 * 	- Implementa pilha (Stack)
+	 *  - Last in / First out (lifo) 
+	 */
+}
---
+++ Java-Curso/src/colecoes/SetExemplo.java
@@ -0,0 +1,54 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExemplo {
+
+	/**
+	 * Pode ser heterogêneo
+	 * Pode ser Homogênio
+	 * Não aceita obj. duplicados
+	 * Pode ser ordenado
+	 * Não é indexado
+	 */
+	public static void main(String[] args) {
+		
+		HashSet conjunto = new HashSet(); // Declarando Set
+		
+		conjunto.add(1.2); // Adicionando valores
+		conjunto.add(true);
+		conjunto.add("Teste");
+		conjunto.add(1);
+		conjunto.add('x');
+		
+		System.out.println("Tamanho é " + conjunto.size()); // Verificando o tamanho do HashSet
+		
+		System.out.println(conjunto.remove("teste")); // Removendo items 
+		System.out.println(conjunto.remove("Teste"));
+		System.out.println(conjunto.remove('x'));
+		
+		System.out.println("Tamanho é " + conjunto.size()); 
+		
+		System.out.println(conjunto.contains('x')); // verifica se tem o x dentro do set // Retorna boolean
+		System.out.println(conjunto.contains(1));
+		System.out.println(conjunto.contains(true));
+		
+		Set nums = new HashSet(); // Declarando HashSet utilizando Set
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		System.out.println(nums);
+		System.out.println(conjunto);
+		
+		// conjunto.addAll(nums); // União entre dois conjuntos
+		
+		conjunto.retainAll(nums); // Retem o que está no outro conjunto // Interseção! 
+		System.out.println(conjunto);
+		
+		conjunto.clear(); // Limpa o HashSet
+		System.out.println(conjunto);
+		
+	}
+}
