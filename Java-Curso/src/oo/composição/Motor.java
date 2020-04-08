81e087c060aaada9d78e68932aff828070457a9e
GussAlves <gus.alves.araujo@gmail.com>
2020-04-08 12:06:10 -0300

    Criando uma classe Carro, e fazendo ele funcionar (Relação 1 para 1)

---
+++ Java-Curso/src/oo/composição/Motor.java
@@ -0,0 +1,15 @@
+package oo.composição;
+
+public class Motor {
+
+	boolean ligado = false;
+	double fatorInjecao = 1;
+	
+	int giros() {
+		if(!ligado) { // Verificando se o motor está ligado
+			return 0;
+		} else { // Se estiver ligado ira contar 3000 giros por injeção
+			return (int) Math.round(fatorInjecao * 3000);
+		}
+	}
+}
