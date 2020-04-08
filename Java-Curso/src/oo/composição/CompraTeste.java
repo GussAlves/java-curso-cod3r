4ffb6cfd496a95d6dd2b792a28bd0800a415a3ca
GussAlves <gus.alves.araujo@gmail.com>
2020-04-08 16:27:54 -0300

    Criando uma relação de 1 para n, e realizando testes

---
+++ Java-Curso/src/oo/composição/CompraTeste.java
@@ -0,0 +1,32 @@
+package oo.composição;
+
+public class CompraTeste {
+
+	public static void main(String[] args) {
+		
+		Compra compra1 = new Compra();
+		compra1.cliente = "João Pedro";		
+		compra1.adicionarItem("Caneta", 20, 7.45);
+		compra1.adicionarItem(new Item("Borracha", 12, 2.89));
+		compra1.adicionarItem(new Item("Caderno", 3, 18.79));
+		
+		System.out.println("Quantidade de itens " + compra1.itens.size()); // Verificando quantidade de itens da compra
+		System.out.println("O valor total da compra é de R$ " + compra1.obterValorTotal()); // Retornando o valor total da compra
+	
+		// Só para mostrar a realação bidirecional!!!
+		double total = compra1.itens.get(0).compra.itens.get(1).compra.obterValorTotal();
+		System.out.println("O total é " + total);
+		
+		System.out.println("*****");
+		System.out.println("Compra do cliente " + compra1.cliente);
+		System.out.println("Itens: \n");
+		for(Item itens: compra1.itens) {
+			System.out.println("Produto: " +  itens.nome);
+			System.out.println("Quantidade: " +  itens.quantidade);
+			System.out.println("Preço S$ " +  itens.preco);
+			System.out.println("**");
+		}
+		System.out.println("Total a pagar: " + total);
+		System.out.println("*****");
+	}
+}
