5e47bbf095f05965c75c5b9bca6e28b901dd2368
GussAlves <gus.alves.araujo@gmail.com>
2020-04-19 13:14:18 -0300

    // Predicate le um tipo e retorna um boolean

---
+++ Java-Curso/src/lambdas/Predicado.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+	
+	// Predicate le um tipo e retorna um boolean 
+
+	public static void main(String[] args) {
+		
+		// Declarando um predicado do tipo Produto, inserindo a condição de preço maior igual 
+		Predicate<Produto> isCaro = prod -> (prod.PRECO * (1 - prod.DESCONTO)) >= 750;
+		
+		Produto produto = new Produto("Notebook", 3893.89, 0.15);
+		System.out.println(isCaro.test(produto));
+	}
+}
