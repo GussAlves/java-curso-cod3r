18993ff09000d441989dd1ecde62ebb7eaf66592
GussAlves <gus.alves.araujo@gmail.com>
2020-04-19 13:17:27 -0300

    // Consumer le um Tipo e não retorna nada

--- Java-Curso/src/lambdas/Consumidor.java
+++ Java-Curso/src/lambdas/Consumidor.java
@@ -14,7 +14,7 @@ public class Consumidor {
 		Consumer<Produto> imprimirNome = p -> System.out.println(p.NOME + "!!!");
 
 		Produto p1 = new Produto("Caneta", 12.34, 0.09);
-		imprimirNome.accept(p1); // Utilizando imprimir para mostrar objeto
+//		imprimirNome.accept(p1); // Utilizando imprimir para mostrar objeto
 
 		Produto p2 = new Produto("Notebook", 2989.99, 0.25);
 		Produto p3 = new Produto("Caderno", 19.90, 0.03);
@@ -22,9 +22,12 @@ public class Consumidor {
 		Produto p5 = new Produto("Lapis", 4.39, 0.19);
 		
 		List<Produto> produtos = Arrays.asList(p1, p2, p3, p4, p5); // Lista produtos 
+		System.out.println();
 		produtos.forEach(imprimirNome); // Foreach com a Lambda criada Imprimir
+		System.out.println();
 		produtos.forEach(p -> System.out.println(p.PRECO)); // Utilizando a expressar diretamente 
 		// Utilizando Method Reference (Para usar assim temos que declarar um toString
+		System.out.println();
 		produtos.forEach(System.out::println); 
 	}
 }
