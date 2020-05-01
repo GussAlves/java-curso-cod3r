58ff527a78ff2ce45af8862abbac8b3cb5d8976f
GussAlves <gus.alves.araujo@gmail.com>
2020-05-01 12:33:07 -0300

    Aprendendo como usar o bloco finally

---
+++ Java-Curso/src/excecao/Finally.java
@@ -0,0 +1,21 @@
+package excecao;
+
+import java.util.Scanner;
+
+public class Finally {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println(7 / sc.nextInt());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally { // Finally sempre será executado 
+			System.out.println("Finally...");
+			sc.close();			
+		}
+		
+		System.out.println("Fim :)");
+	}
+}
